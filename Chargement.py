ed32f676d30a7d3b359d4ced395b91f577892253
makdim <makdim@enseirb.fr>
2013-05-17 17:34:45 +0200

    " "

--- Chargement.py
+++ Chargement.py
@@ -3,7 +3,7 @@ import math as mth
 import MSTKruskal as K
 import numpy as np
 
-def calculDisEuclidienne(path):
+def EuclideanDistMatrix(path):
 	
 	f = open(path,"r")
 	nodes=[]
@@ -16,14 +16,13 @@ def calculDisEuclidienne(path):
 		Y.append(int(words[2]))
 	
 	n=len(nodes)
-	M=n*[0]
-	for i in range(n):
-		M[i] = n*[0]
+	M=[n*[0] for i in range(n)]
+
 	for i in range(n):
 		for j in range(n):
 			M[i][j]=mth.sqrt((X[i]-X[j])**2+(Y[i]-Y[j])**2)
 	return np.array(M)
 	
-F=K.MinimumSpanningTree(calculDisEuclidienne("./dev/data/n10-1"))
+F=K.MinimumSpanningTree(EuclideanDistMatrix("./dev/data/n10-1"))
 print(F)
 print(K.convertToAdjacent(F))
